e9d75a4f7f9c03418cb4c0524d667424288476b1
armndhc <95389460+armndhc@users.noreply.github.com>
2026-08-27 14:52:11 -0600

    Final
    Cambios FInales del navbar

--- src/script/components/navbar.js
+++ src/script/components/navbar.js
@@ -4,61 +4,70 @@ class NavbarComponent extends HTMLElement {
     }
 
     connectedCallback() {
+        // Usa root="true" cuando el componente se coloque en el index.html de la raíz.
+        // Sin ese atributo (o root="false") asume que se usa desde src/pages/*.html.
+        const isRoot = this.getAttribute('root') === 'true';
+
+        const homeLink = isRoot ? 'index.html' : '../../index.html';
+        const imgBase = isRoot ? 'src/images/Encabezado' : '../images/Encabezado';
+        const pageBase = isRoot ? 'src/pages' : '../pages';
+
         this.innerHTML = `
-        <nav class="nav-container">
+        <nav class="nav-container" id="main-navbar">
             <div class="main-nav">
                 <div class="nav-logos">
-                    <img src="../images/Encabezado/FI2.png" alt="Logo UNAM">
-                    <img src="../images/Encabezado/Logo TD_ColorNegro_CMYK.png" alt="Logo Facultad de Ingeniería">
+                    <img src="${imgBase}/FIBlanco.png" alt="Logo UNAM">
+                    <img src="${imgBase}/Logo TD_Blanco_CMYK.png" alt="Logo Facultad de Ingeniería">
+                    <img src="${imgBase}/DIE Blanco.png" alt="Logo DIE">
                 </div>
                 <ul class="nav-list" id="main-menu">
-                    <li><a href="../../index.html" class="">Inicio</a></li>
+                    <li><a href="${homeLink}" class="">Inicio</a></li>
                     <li class="dropdown">
                         <a href="#">Acerca de</a>
                         <div class="dropdown-content">
-                            <a href="../pages/organizacion-organigrama.html">Organigrama</a>
+                            <a href="${pageBase}/organizacion-organigrama.html">Organigrama</a>
                         </div>
                     </li>
                     <li class="dropdown">
                         <a href="#">Quioscos PC Puma </a>
                         <div class="dropdown-content">
-                            <a href="../pages/quioscos.html">Quioscos</a>
-                            <a href="../pages/personal.html">&iquest;Quienes somos?</a>
-                            <a href="../pages/servicios.html">Servicios</a>
-                            <a href="../pages/reglamentotd.html">Reglamento TD</a>
-                            <a href="../pages/registroPCPUMA.html">Registro PC Puma</a>
-                            <a href="../pages/reservacion.html">Reservaci&oacute;n de Salas</a>
+                            <a href="${pageBase}/quioscos.html">Quioscos</a>
+                            <a href="${pageBase}/personal.html">&iquest;Quienes somos?</a>
+                            <a href="${pageBase}/servicios.html">Servicios</a>
+                            <a href="${pageBase}/reglamentotd.html">Reglamento TD</a>
+                            <a href="${pageBase}/registroPCPUMA.html">Registro PC Puma</a>
+                            <a href="${pageBase}/reservacion.html">Reservaci&oacute;n de Salas</a>
                         </div>
                     </li>
                     <li class="dropdown">
-                        <a href="../pages/red-pc-puma.html">Red PC Puma</a>
+                        <a href="${pageBase}/red-pc-puma.html">Red PC Puma</a>
                     </li>
                     <li class="dropdown">
                         <a href="#">Programa de becarios</a>
                         <div class="dropdown-content">
-                            <a href="../pages/becarios-convocatoria.html">Convocatoria</a>
+                            <a href="${pageBase}/becarios-convocatoria.html">Convocatoria</a>
                             <a href="https://www.ingenieria.unam.mx/transformaciondigital/IT-Pro/registro/index.php/cargaSolicitudRegistro">Registro</a>
-                            <a href="../pages/becarios-generaciones.html">Generaciones</a>
+                            <a href="${pageBase}/becarios-generaciones.html">Generaciones</a>
                         </div>
                     </li>
                     <li class="dropdown">
-                        <a href="../pages/cursos-temario.html">Cursos</a>
+                        <a href="${pageBase}/cursos-temario.html">Cursos</a>
                         <div class="dropdown-content">
-                            <a href="../pages/cursos-convocatoria.html" class="dropdown-link">Convocatoria</a>
+                            <a href="${pageBase}/cursos-convocatoria.html" class="dropdown-link">Convocatoria</a>
                             <a href="https://docs.google.com/forms/d/e/1FAIpQLSdvfc-D4T5L0GSAmAHAEK3r28_zH1a3IsBziNIyNgvx1yVkTQ/viewform" target="_blank">Registro</a>
-                            <a href="../pages/cursos-costos.html" class="dropdown-link">Costos</a>
+                            <a href="${pageBase}/cursos-costos.html" class="dropdown-link">Costos</a>
                         </div>
                     </li>
                     <li class="dropdown">
                         <a href="#">Plataformas Educativas</a>
                         <div class="dropdown-content">
-                            <a href="../pages/plataformas-educafi.html" class="dropdown-link">EDUCAFI</a>
-                            <a href="../pages/plataformas-google-suite.html" class="dropdown-link">Google Suite</a>
-                            <a href="../pages/plataformas-office-365.html" class="dropdown-link">Office 365</a>
+                            <a href="${pageBase}/plataformas-educafi.html" class="dropdown-link">EDUCAFI</a>
+                            <a href="${pageBase}/plataformas-google-suite.html" class="dropdown-link">Google Suite</a>
+                            <a href="${pageBase}/plataformas-office-365.html" class="dropdown-link">Office 365</a>
                         </div>
                     </li>
                     <li class="dropdown">
-                        <a href="../pages/estadisticas-pc-puma.html">Estad&iacute;sticas PC Puma</a>
+                        <a href="${pageBase}/estadisticas-pc-puma.html">Estad&iacute;sticas PC Puma</a>
                     </li>
                 </ul>
             </div>
